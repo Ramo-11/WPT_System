68b6b1706dcc8b55f5511b5982789a00dd5d6c02
Omar Abdelalim <omarh5877@gmail.com>
2022-04-17 11:36:45 -0400

    changed the title of program

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import GUI.Frames.*;
 
 public class Main {
     public static void main (String[] args) {
-        new MainFrame("Simulation of Wireless Power Transfer System", null);
+        new MainFrame("Power Loss Demonstrator", null);
     }
 }
